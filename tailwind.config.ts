79f2b2b7bdb6fbebce60bc4e092949e4a5cd832a
Jedd Niñonuevo <jed@jasetech.ca>
2024-10-04 14:51:30 +0800

    Initialize Flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,29 @@
+import flowbitePlugin from "flowbite/plugin";
+
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+	darkMode: "selector",
+	theme: {
+		extend: {
+			colors: {
+				// flowbite-svelte
+				primary: {
+					"50": "#faf5ff",
+					"100": "#eff6ff",
+					"200": "#dbeafe",
+					"300": "#bfdbfe",
+					"400": "#93c5fd",
+					"500": "#60a5fa",
+					"600": "#3b82f6",
+					"700": "#2563eb",
+					"800": "#1d4ed8",
+					"900": "#1e40af",
+				},
+			},
+		},
+	},
 
-  theme: {
-    extend: {}
-  },
-
-  plugins: []
+	plugins: [flowbitePlugin],
 } as Config;
